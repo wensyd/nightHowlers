0c32a9ae17fa1308b5fa7463d268c6fa8cc872a6
wensydesousa <wensys013@gmail.com>
2022-02-26 20:12:12 -0800

    updated contentful blogs

--- src/Client.js
+++ src/Client.js
@@ -2,7 +2,7 @@ import * as contentful from 'contentful'
 
 export const client = contentful.createClient({
         space: process.env.REACT_APP_SPACE_ID,
-        accessToken: process.env.REACT_APP_ACCESS_TOKEN
+        accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 
 })
 
--- src/components/blog/bloggrid/Content.js
+++ src/components/blog/bloggrid/Content.js
@@ -1,37 +1,81 @@
-import React from "react";
+import React, {useState, useEffect, useCallback} from "react";
+import {client} from '../../../Client'
 
 const Content = () => {
+    const [blogPosts, setBlogPosts]= useState([])
+
+    const cleanBlogPosts = useCallback((rawData) => {
+        const cleanBlogPosts= rawData.map((posts) => {
+            const {sys, fields}= posts
+            const {id} = sys
+            const blogTitle = fields.title
+            const blogImage = fields.featuredImage.fields.file.url
+            const blogDescription = fields.description
+            const blogBody = fields.body
+            const slug = fields.slug
+            const updatedPosts = {id, blogTitle, blogImage, blogDescription, blogBody, slug}
+            return updatedPosts
+        })
+        setBlogPosts(cleanBlogPosts)
+    }, [])
+
+    const getBlogPosts = useCallback( async()=>{
+        try{
+            const response = await client.getEntries({content_type: 'blogPost', order: 'sys.createdAt'})
+            const responseData = response.items
+           if(responseData){
+               cleanBlogPosts(responseData)
+           }else{
+               setBlogPosts([])
+           }
+        } catch(error){
+            console.log(error)
+        }
+    }, [cleanBlogPosts])
+
+    useEffect(()=>{
+        getBlogPosts()
+    },[getBlogPosts])
+
+console.log(blogPosts)
+
+
+
+
+
+
+
     return (
         <>
-    
-            <main id="body-content">
+        <main id="body-content">
                 <header className="wide-mt-0 blogsHeader">
                     <div >
                         <img src="images/banner_2.jpg" className="img-fluid blogimg d-block w-100 h-100 p-0" alt=""/>
                     </div>
-
                 </header>
                 <section className="wide-tb-5 pb-5">
-                    <div ><h1 className="blogh1 my-5 display-5" align="center">Our Blog</h1></div>
+                <div ><h1 className="blogh1 my-5 display-5" align="center">Our Blog</h1></div>
                 <hr className="w-75"/>
-
-                    <div className="container pos-rel">
-                        <div id="js-grid-masonry" className="cbp">
-                            
-                            <div className="cbp-item">
-                                <div className="blog-warp text-left">
-                                    <img src="images/blog_img_2.jpg" alt="" className="rounded" />
-                                    
-                                    <h4 className="h4-md mb-3" align="center"><a href="blogsingle">Title</a></h4>
-                                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiumg</p>
-                                    <a href="blogsingle" className="btn-theme bg-navy-blue no-shadow">Read More <i className="icofont-rounded-right"></i></a>
-                                </div>
-                            </div>
-                         
-                        </div>
-                    </div>
                 </section>
-            </main>
+                </main>
+                <div className="row">
+        {blogPosts.map((item)=> {
+            
+           const { id, slug, blogTitle, blogDescription, blogBody, blogImage, index }= item
+            return (
+                                <div key ={index} className="card col-md-6">
+                                    <img src={blogImage} alt="" className="card-img-top rounded img-fluid" />
+                                    
+                                    <h4 className="card-title h4-md mb-3" align="center"><a href={slug}>{blogTitle}</a></h4>
+                                    <p className="card-text">{blogDescription}</p>
+                                    <a href={slug} className="btn btn-primary btn-theme bg-navy-blue no-shadow">Read More <i className="icofont-rounded-right"></i></a>
+                                </div>
+                        
+                     
+                
+            )
+            })}
+            </div>
         </>
     );
 };
