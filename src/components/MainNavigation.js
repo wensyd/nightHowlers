582d59f79f54fafedd3b1ab64ab7ad1a6076f4bc
wensydesousa <wensys013@gmail.com>
2022-03-03 18:40:49 -0800

    single blog connected

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -27,7 +27,7 @@ function MainNavigation() {
         {/** Blog */}
         <Route path="/blog" exact={true} component={BlogGrid} />
 
-        <Route path="/:slug" exact={true} component={BlogSingle}/>
+        <Route path="/blogsingle/:slug" exact={true} component={BlogSingle}/>
         {/** invalid routes redirection */}
         <Route component={NotFound} />
       </Switch>
--- src/components/blog/blogsingle/Content.js
+++ src/components/blog/blogsingle/Content.js
@@ -1,22 +1,41 @@
 import {React, useEffect, useState, useCallback} from "react";
+import { useParams } from "react-router";
 import {client} from '../../../Client'
 
 const Content = () => {
-
+    const params = useParams();
+   
     const [singleBlog, setSingleBlog] = useState([])
 
     const cleanSingleBlog = useCallback((rawData) => {
-       const cleanSingleBlog = rawData.map((post) => {
-            const {sys, fields} = post
-            const {id} = sys
-            const slug = fields.slug
-            const blogTitle = fields.title
-            const blogImage = fields.featuredImage.fields.file.url
-            const blogDescription = fields.description
-            const blogBody = fields.body
-            const updateSingleBlog = {id, blogTitle, blogImage, blogDescription, blogBody, slug}
-            return updateSingleBlog
-        })
+   
+       const cleanSingleBlog = rawData
+       .filter((post) =>{
+          
+     
+           if(post.fields.slug === params.slug) {
+              
+              
+              
+               const {sys, fields} = post
+               
+               const {title, slug, description, body} = fields
+               const featuredImage = fields.featuredImage.fields.file.url
+            
+            
+               
+                const updateSingleBlog = {
+                    "title":title,
+                    "slug":slug, 
+                    "description": description,
+                    "body": body,
+                    "featuredImage": featuredImage}
+                
+                return updateSingleBlog
+           }
+       })
+        console.log(cleanSingleBlog)
+      
         setSingleBlog(cleanSingleBlog)
     }, [])
 
@@ -25,8 +44,9 @@ const Content = () => {
         try {
             const response = await client.getEntries(
                 {content_type: 'blogPost',
-                'fields.slug' : slug
+                'fields' :{}
             })
+          
             const responseData = response.items
             if(responseData) {
                 cleanSingleBlog(responseData)
@@ -39,10 +59,11 @@ const Content = () => {
     }, [cleanSingleBlog])
 
     useEffect(() => {
+        
         getSingleBlog()
     }, [getSingleBlog])
 
-    console.log(singleBlog)
+    // console.log(singleBlog)
 
     return (
         <>
@@ -52,10 +73,15 @@ const Content = () => {
                 </header>
                 </main>
             <div className="container">
-                {singleBlog.map((item) => {
-                    const { id, slug, blogTitle, blogImage, blogDescription, blogBody} = item
+                {singleBlog.map((post) => {
+                    
+                const slug = post.fields.slug
+                const blogTitle = post.fields.title
+                const blogImage = post.fields.featuredImage.fields.file.url
+                const blogDescription = post.fields.description
+                const blogBody = post.fields.body
                     return(
-                                        <div  key = {id} >
+                                        <div className="container">
                                             <img src={blogImage} alt="" className="rounded mb-4" />
                                           
                                             <h4 className="h4-md mb-3 fw-7 txt-blue" align="center">{blogTitle}</h4>
